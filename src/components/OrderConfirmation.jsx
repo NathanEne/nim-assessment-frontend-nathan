e146ff9882efe57a0757ef8996dfd3b28bb50d33
NathanEne <nathan@enerson.ca>
2023-07-16 18:12:12 -0600

    Navigate to Confirmation Page

--- src/components/OrderConfirmation.jsx
+++ src/components/OrderConfirmation.jsx
@@ -1,35 +1,28 @@
-import React, { useEffect, useState } from "react";
-import styles from "./styles/Order.module.css";
-import ConfirmationPage from "./ConfirmationPage";
+function OrderConfirmation({ order }) {
+  const { name, address, id, items } = order;
 
-function OrderConfirmation(props){
-    const name = props.order.name;
-    const address = props.order.address;      
-    const id = props.order.id;
-      
-    const items = [];
-    let i = 0;
-    while (i < props.order.items.length) {
-        items[i] = props.order.items[0].item.name + ", " +props.order.items[0].quantity
-        console.log(items[i])
-        i++;
-    }
-        
+  let i = 0;
+  let totalPrice = 0;
+  const itemsNew = [];
+  while (i < items.length) {
+    itemsNew[i] = `${items[i].item.name}, ${items[i].quantity}`;
+    totalPrice += items[i].item.price * items[i].quantity;
+    i += 1;
+  }
 
+  return (
+    <div>
+      <h1>Order Confirmation</h1>
 
-    
-    return (
-        <>
-            <h1>Order Confirmation</h1>
-            <ul>
-                <li>Name: {name}</li>
-                <li>Address: {address}</li>
-                <li>Items: {items}</li>
-                <li>Id: {id}</li>
-            </ul>
-        </>
-
-    );
+      <ul>
+        <li> Name: {name}</li>
+        <li>Address: {address}</li>
+        <li>Items: {itemsNew}</li>
+        <li>Order Id: {id}</li>
+        <li>Sub Total: {totalPrice}</li>
+      </ul>
+    </div>
+  );
 }
 
 export default OrderConfirmation;
